18bc38b89d2bca6eb420f3ada6f36bb6603e9a4b
ShengYuCheng <sean.sheng.yu.cheng@gmail.com>
2024-11-22 17:27:18 +0800

    added some

--- game.py
+++ game.py
@@ -1,11 +1,21 @@
 from block import *
 from player import *
 
+# game status:
+# wait commnd
+# do animation
+
+
+class GameStatus:
+    WAIT_PLAYER_COMMAND = 0
+    DOING_ANIMATION = 1
+
 class Game:
     def __init__(self, blocks, players):
         self.block_amount = len(blocks)
         self.blocks = blocks
         self.player_amount = len(players)
         self.players = players
-    def movePlayerTo(self, index, position):
-        self.players[index] 
+        self.status = GameStatus.WAIT_PLAYER_COMMAND
+    def rollDice(self, ):
+        
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-# proposal: image
-
-# initial: money 
-# one die
-
-
-
-# Example file showing a circle moving on screen
+# Example file showing a basic pygame "game loop"
 import pygame
 
 # pygame setup
@@ -13,9 +6,6 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -27,24 +17,11 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
