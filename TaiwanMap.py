9eb5fb0d1d95ad7bb934d3c404effb7b91b3789f
ShengYuCheng <sean.sheng.yu.cheng@gmail.com>
2024-12-04 18:18:45 +0800

    modified

--- TaiwanMap.py
+++ TaiwanMap.py
@@ -1,4 +1,7 @@
 from EventCardDeck import *
+from block import *
+from constant import *
+from player import *
 import pygame
 
 # chance card deck
@@ -6,6 +9,7 @@ chanceCardDeck = []
 
 # chill guy
 image = pygame.image.load("Assets/EventCards/chill guys.jpg")
-def chillGuy(**kwargs):
-    kwargs['']
+def chillGuy(block: BLOCK, blocks: list[BLOCK], now_player_index: int, players: list[Player]):
+    players[now_player_index].stop_round = 1
+
 chanceCardDeck.append(EventCard(image, 10, False))
