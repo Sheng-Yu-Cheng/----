392f07ab5b382a2574cee1137c661cd3352217c2
ShengYuCheng <sean.sheng.yu.cheng@gmail.com>
2024-12-17 21:24:41 +0800

    commititeddd

--- TaiwanMap.py
+++ TaiwanMap.py
@@ -556,10 +556,10 @@ def generateGame() -> Game:
         PlayerIcon(pygame.transform.scale(pygame.image.load("Assets/TaiwanBoard/PlayerIcons/pop.jpg"), (100, 100)), (535, 105))
     ]
     players = [
-        Player("Player 1", 0, player_token[0], player_icons[0], StockMarketAccount(market), [Rabbit(), Rabbit(), Rabbit(), Rabbit()], balance = 25000, health_point = 100), 
-        Player("Player 2", 1, player_token[1], player_icons[1], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100), 
-        Player("Player 3", 2, player_token[2], player_icons[2], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100), 
-        Player("Player 4", 3, player_token[3], player_icons[3], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100)
+        Player("pink", 0, player_token[0], player_icons[0], StockMarketAccount(market), [Barrier(), Barrier(), Barrier(), Barrier()], balance = 25000, health_point = 100), 
+        Player("orange", 1, player_token[1], player_icons[1], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100), 
+        Player("green", 2, player_token[2], player_icons[2], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100), 
+        Player("blue", 3, player_token[3], player_icons[3], StockMarketAccount(market), [Rabbit(), Turtle()], balance = 25000, health_point = 100)
     ]
     game = Game(
         (1280, 720), 
